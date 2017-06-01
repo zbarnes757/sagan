b279e065444067e9c9d42aff7ba7bb68a0e19eb9
Zac Barnes <zac.barnes@toyotaconnected.com>
2017-06-01 14:14:02 -0500

    finish testing methods

--- spec/sagan/api_spec.exs
+++ spec/sagan/api_spec.exs
@@ -43,9 +43,39 @@ defmodule Sagan.APISpec do
   end
 
   describe "get/1" do
-    it "should get from Cosmos DB using the url provided"
-    it "should error when status code comes back other than 2XX"
-    it "should error when HTTPoison fails"
-    it "should error for any other case"
+    it "should get from Cosmos DB using the url provided" do
+      use_cassette "get_document", custom: true do
+        assert {:ok, resp} = API.get("dbs/test/colls/testDocs/docs/foo")
+
+        expect resp["id"]  |> to(eq "foo")
+        expect resp["bar"] |> to(eq "baz")
+        expect resp        |> to(have_key "_rid")
+        expect resp        |> to(have_key "_self")
+        expect resp        |> to(have_key "_etag")
+        expect resp        |> to(have_key "_attachments")
+        expect resp        |> to(have_key "_ts")
+      end
+    end
+
+    it "should return ok and the message when status code comes back other than 2XX" do
+      use_cassette "get_document_fail", custom: true do
+        assert {:ok, resp} = API.get("dbs/test/colls/testDocs/docs/foo")
+
+        expect resp["code"]     |> to(eq "NotFound")
+        expect resp["message"]  |> to(be_binary())
+      end
+    end
+
+    it "should error when HTTPoison fails" do
+      with_mock HTTPoison, [get: fn(_, _, _) -> {:error, %HTTPoison.Error{reason: "fail"}} end] do
+        assert {:error, "fail"} = API.get("dbs/test/colls/testDocs/docs/foo")
+      end
+    end
+
+    it "should error for any other case" do
+      with_mock HTTPoison, [get: fn(_, _, _) -> "not a valid option" end] do
+        assert {:error, "Unknown error occured during api call."} = API.get("dbs/test/colls/testDocs/docs/foo")
+      end
+    end
   end
 end
